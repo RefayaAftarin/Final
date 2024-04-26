582fd8e141c4e410617c495f69bae45b8a5062f3
RefayaAftarin <refayaaftarin2018@gmail.com>
2024-04-26 10:56:54 +0600

    7th commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function calculate(){
     var search=document.getElementById("search").value;
     document.getElementById("search").value="";
-    var url= `www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+    var url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
     fetch(url)
     .then (res=>res.json())
     .then(show=>show(data.meals))
