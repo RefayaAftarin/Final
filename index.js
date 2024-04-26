8244f7e99fad1400aedf23b01c42e5bb0a115dce
RefayaAftarin <refayaaftarin2018@gmail.com>
2024-04-26 09:55:09 +0600

    4th commit

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ function calculate(){
     .then (res=>res.json())
     .then(show=>show(data.meals))
 
+}
+function show(data){
+    oldcontent=document.getElementById("container1");
+    oldcontent.textcontent="";
 
+    for(var i=1;i<data.length[5];i++){
+        console.log(data);
+        var newDiv=document.createElement("div");
+        newDiv.innerHTML=`Meal Title: <b>${data[i-1].strMeal}</b> <br>`;
+        newDiv.classList.add("innerStyle");
+        oldcontent.appendchild(newDiv);
+    }
 }
